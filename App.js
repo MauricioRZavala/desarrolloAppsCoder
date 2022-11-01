d5d4aa43c8e5e3f11eb449c10bc92d7fef4c9376
MauricioRZavala <mau_ole@hotmail.com>
2022-10-31 21:52:46 -0600

    Desafio curso desarrollo de apps

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <TextInput placeholder="Agregar item a la lista" style={styles.inputItem} />
+        <Button title='Agregar' />
+      </View>
+
+      <View style={styles.itemsContainter}>
+      <TextInput placeholder='Item1' style={styles.item} />
+      <TextInput placeholder='Item2' style={styles.item} />
+      
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    padding: 30, 
+    marginTop: 50 
   },
+  itemContainer:
+  {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputItem: {
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1,
+    width: 90
+  },
+  itemsContainter:{
+    flexDirection:'column',
+    alignItems:'center',
+    backgroundColor: 'lightblue',
+    
+  },
+  item:{
+    marginTop: 5,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    
+
+  }
+
 });
